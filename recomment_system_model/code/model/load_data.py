02115d49a03ebc24e839effc7cea53efca43a8c1
vutriancode <vutrian576@gmail.com>
2020-12-14 15:37:22 +0700

    commit

--- recomment_system_model/code/model/load_data.py
+++ recomment_system_model/code/model/load_data.py
@@ -1,3 +1,9 @@
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from math import sqrt
+import torch
+import numpy as np
+
 from CONFIG import *
 
 class Training:
@@ -5,7 +11,7 @@ class Training:
     def __init__(self, model):
         self.model = model
 
-    def train(self, train_loader, optimizer, epoch, best_rmse, best_mae, device="cpu"):
+    def train(self, train_loader, optimizer,epoch, best_rmse, best_mae, device="cpu"):
 
         running_loss = 0.0
         for i, data in enumerate(train_loader, 0):
@@ -15,8 +21,25 @@ class Training:
             loss.backward(retain_graph=True)
             optimizer.step()
             running_loss += loss.item()
-            if i % 1 == 0:
+            if i % 5000 == 0:
                 print('[%d, %5d] loss: %.3f, The best rmse/mae: %.6f / %.6f' % (
                     epoch, i, running_loss / 1, best_rmse, best_mae))
                 running_loss = 0.0
         return 0
+    #print(train_u)
+    def test(self,test_loader, device):
+        self.model.eval()
+        tmp_pred = []
+        target = []
+        with torch.no_grad():
+            for test_u, test_v, tmp_target in test_loader:
+
+                test_u, test_v, tmp_target = test_u.to(device), test_v.to(device), tmp_target.to(device)
+                val_output = self.model.forward(test_u, test_v)
+                tmp_pred.append(list(val_output.data.cpu().numpy()))
+                target.append(list(tmp_target.data.cpu().numpy()))
+        tmp_pred = np.array(sum(tmp_pred, []))
+        target = np.array(sum(target, []))
+        expected_rmse = sqrt(mean_squared_error(tmp_pred, target))
+        mae = mean_absolute_error(tmp_pred, target)
+        return expected_rmse, mae
